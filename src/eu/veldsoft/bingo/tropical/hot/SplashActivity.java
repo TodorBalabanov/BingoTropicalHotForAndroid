ca014ff561b556a41b4118e16427e6dedd717f8a
TodorBalabanov <todor.balabanov@gmail.com>
2016-12-12 19:02:01 +0200

    Icon was replaced and message bar text componet was added.

--- src/eu/veldsoft/bingo/tropical/hot/SplashActivity.java
+++ src/eu/veldsoft/bingo/tropical/hot/SplashActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 /**
  * Splash screen window.
@@ -31,17 +32,20 @@ public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_splash);
 
-//		/*
-//		 * Activate JavaScript.
-//		 */
-//		((WebView) findViewById(R.id.ads)).getSettings().setJavaScriptEnabled(true);
-//
-//		/*
-//		 * Load local web page as banner holder.
-//		 */
-//		((WebView) findViewById(R.id.ads)).loadUrl("file:///android_asset/banner.html");
+		// /*
+		// * Activate JavaScript.
+		// */
+		// ((WebView)
+		// findViewById(R.id.ads)).getSettings().setJavaScriptEnabled(true);
+		//
+		// /*
+		// * Load local web page as banner holder.
+		// */
+		// ((WebView)
+		// findViewById(R.id.ads)).loadUrl("file:///android_asset/banner.html");
 
 		/*
 		 * Get splash screen timeout.
